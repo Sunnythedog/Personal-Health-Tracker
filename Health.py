154760ddf11b1d9c5a51f35e2af98604cd9d2bc2
Sunnythedog <16982432+Sunnythedog@users.noreply.github.com>
2018-01-28 19:57:17 -0500

    Update Health.py

--- Health.py
+++ Health.py
@@ -1,88 +1,162 @@
-#Health Data
+# health.py started 12/31/17 @ 12:40 pm revision 0.1
+# Created to track my health
+
 import sqlite3
+import time
 
-db = sqlite3.connect('test4.sqlite')
-db.execute("""CREATE TABLE IF NOT EXISTS test4 
-            (id INTEGER,
-            date TEXT,
-            test TEXT, 
-            range TEXT,
-            result REAL)""")
-db.execute("""INSERT INTO test4 
-            (id, date, test, range, result) VALUES (1, '2017-10-30', 'WBC', '3.9-10.3 K UL', 6.1)""")
-db.execute("""INSERT INTO test4 
-            (id, date, test, range, result) VALUES (2, '2017-10-30', 'RBC', '4.34-5.61 M UL', 5.12)""")
-db.execute("""INSERT INTO test4 
-            (id, date, test, range, result) VALUES (3, '2017-10-30', 'HGB', '12.6-17.0 g dbl', 15.6)""")
-db.execute("""INSERT INTO test4 
-            (id, date, test, range, result) VALUES (4, '2017-10-30', 'HCT', '37.8-51%', 45.5)""")
-db.execute("""INSERT INTO test4 
-            (id, date, test, range, result) VALUES (5, '2017-10-30', 'MCV', '82.0-98.4 fL', 88.8)""")
-db.execute("""INSERT INTO test4 
-            (id, date, test, range, result) VALUES (6, '2017-10-30', 'MCH', '25.7-33.8 pg', 30.4)""")
-db.execute("""INSERT INTO test4 
-            (id, date, test, range, result) VALUES (7, '2017-10-30', 'MCHC', '32.0-36.0 G DL', 34.3)""")
-db.execute("""INSERT INTO test4 
-            (id, date, test, range, result) VALUES (8, '2017-10-30', 'RDW', '11.5-15.5%', 12.4)""")
-db.execute("""INSERT INTO test4 
-            (id, date, test, range, result) VALUES (9, '2017-10-30', 'PLT', '150-400 K UL', 253)""")
-db.execute("""INSERT INTO test4 
-            (id, date, test, range, result) VALUES (10, '2017-10-30', 'MPV', '7.4-10.4 fL', 7.7)""")
-db.execute("""INSERT INTO test4 
-            (id, date, test, range, result) VALUES (11, '2017-10-30', 'Glu', '80-117 mg dL', 109)""")
-db.execute("""INSERT INTO test4 
-            (id, date, test, range, result) VALUES (12, '2017-10-30', 'BUN', '7-25 mg dL', 9)""")
-db.execute("""INSERT INTO test4 
-            (id, date, test, range, result) VALUES (13, '2017-10-30', 'Creat', '0.5-1.5 mg dL', 1.2)""")
-db.execute("""INSERT INTO test4 
-            (id, date, test, range, result) VALUES (14, '2017-10-30', 'BUN/Creat', '6-20', 8)""")
-db.execute("""INSERT INTO test4 
-            (id, date, test, range, result) VALUES (15, '2017-10-30', 'Calcium', '8.5-10.8 mg dL', 9.5)""")
-db.execute("""INSERT INTO test4
-            (id, date, test, range, result) VALUES (16, '2017-10-30', 'Na', '135-148 meq L', 141)""")
-db.execute("""INSERT INTO test4
-            (id, date, test, range, result) VALUES (17, '2017-10-30', 'K', '3.5-5.3 men L', 5.0)""")
-db.execute("""INSERT INTO test4
-            (id, date, test, range, result) VALUES (18, '2017-10-30', 'Cl', '100-112 meq L', 102)""")
-db.execute("""INSERT INTO test4
-            (id, date, test, range, result) VALUES (19, '2017-10-30', 'CO2', '23-30 meq L', 27)""")
-db.execute("""INSERT INTO test4
-            (id, date, test, range, result) VALUES (20, '2017-10-30', 'TP', '6.3-7.9 g dl', 7.2)""")
-db.execute("""INSERT INTO test4
-            (id, date, test, range, result) VALUES (21, '2017-10-30', 'Alb', '3.7-4.5 g dl', 4.3)""")
-db.execute("""INSERT INTO test4
-            (id, date, test, range, result) VALUES (22, '2017-10-30', 'A/G', '1.0-2.4', 1.5)""")
-db.execute("""INSERT INTO test4
-            (id, date, test, range, result) VALUES (23, '2017-10-30', 'TBili', '0.0-1.0 mg dL', .4)""")
-db.execute("""INSERT INTO test4
-            (id, date, test, range, result) VALUES (24, '2017-10-30', 'AlkPhos', '31-155 U L', 69)""")
-db.execute("""INSERT INTO test4
-            (id, date, test, range, result) VALUES (25, '2017-10-30', 'AST', '0-40 U L', 19)""")
-db.execute("""INSERT INTO test4
-            (id, date, test, range, result) VALUES (26, '2017-10-30', 'ALT', '0-50 U L', 24)""")
-db.execute("""INSERT INTO test4
-            (id, date, test, range, result) VALUES (27, '2017-10-30', 'Anion', '10-20', 17)""")
-db.execute("""INSERT INTO test4
-            (id, date, test, range, result) VALUES (28, '2017-10-30', 'GFR', '<60 mL min', 65)""")
-db.execute("""INSERT INTO test4
-            (id, date, test, range, result) VALUES (29, '2017-10-30', 'Chol', '0-200 mg dL', 249)""")
-db.execute("""INSERT INTO test4
-            (id, date, test, range, result) VALUES (30, '2017-10-30', 'Trig', '30-150 mg dL', 116)""")
-db.execute("""INSERT INTO test4
-            (id, date, test, range, result) VALUES (31, '2017-10-30', 'HDL', '36-100 mg dL', 58)""")
-db.execute("""INSERT INTO test4
-            (id, date, test, range, result) VALUES (32, '2017-10-30', 'LDL', '0-130', 168)""")
-db.execute("""INSERT INTO test4
-            (id, date, test, range, result) VALUES (33, '2017-10-30', 'VLDL', '0-39', 23)""")
+conn = sqlite3.connect('health.db')
+c = conn.cursor()
 
-cursor = db.cursor()
-cursor.execute("SELECT * FROM test4")
+# Blood Pressure menu selection for database input
+def blood_pressure():
 
-print('{0:10}{1:20}{2:20}{3:10}{4:15}'.format('ID','date','test','range','result'))
-for id, date, test, range, result in cursor:
-    print('{0:3}{1:20}{2:20}{3:10}{4:15}'.format(id,date,test,range,result))
+    def create_table_blood_pressure():
+        c.execute('CREATE TABLE IF NOT EXISTS blood_pressure(unix REAL, systolic INT, diastolic INT, heart_rate INT)')
 
-cursor.close()
-db.close()
+    def blood_pressure_dynamic_data_entry():
+        unix = time.time()
+        systolic = input('enter systolic rate: ')
+        diastolic = input('enter diastolic rate: ')
+        heart_rate = input('enter heart rate: ')
+        c.execute("INSERT INTO blood_pressure (unix, systolic, diastolic, heart_rate) VALUES (?, ?, ?, ?)",
+                  (unix, systolic, diastolic, heart_rate))
+        conn.commit()
 
+    def read_from_db_blood_pressure():
+        c.execute('SELECT * FROM blood_pressure')
+        for row in c.fetchall():
+            print(row)
 
+    create_table_blood_pressure()
+    blood_pressure_dynamic_data_entry()
+    read_from_db_blood_pressure()
+
+    main_menu()
+
+# Blood Test menu selection for database input (33 Values)
+def blood_test():
+
+    def create_table_blood_test():
+        c.execute("""CREATE TABLE IF NOT EXISTS blood_test(date TEXT, WBC REAL, RBC REAL, 
+        HGB REAL, HCT REAL, MCV REAL, MCH REAL, MCH_C REAL, RDW REAL, PLT REAL, MPV REAL, 
+        Glu REAL, BUN REAL, BUNCreat REAL, Calcium REAL, Na REAL, K REAL, Cl REAL, CO2 REAL, 
+        TP REAL, Alb REAL, AG REAL, Tbili REAL, AlkPhos REAL, AST REAL, ALT REAL, Anion REAL, 
+        GFR REAL, Chol REAL, Trig REAL, HDL REAL, LDL REAL, VLDL REAL)""")
+
+    def blood_test_dynamic_data_entry():
+        print('')
+        date = input('Enter date of blood test: ')
+        WBC = input('Enter WBC (White Blood Cell): ')
+        RBC = input('Enter RBC (Red Blood Cell): ')
+        HGB = input('Enter HGB (): ')
+        HCT = input('Enter HCT (Hematocrit): ')
+        MCV = input('Enter MCV (): ')
+        MCH = input('Enter MCH (): ')
+        MCH_C = input('Enter MCHC (): ')
+        RDW = input('Enter RDW (): ')
+        PLT = input('Enter PLT (): ')
+        MPV = input('Enter MPV (): ')
+        Glu = input('Enter Glu (): ')
+        BUN = input('Enter BUN (Blood Urea Nitrogen (kidney function test)): ')
+        BUNCreat = input('Enter BUNCreat (): ')
+        Calcium = input('Enter Calcium: ')
+        Na = input('Enter sodium: ')
+        K = input('Enter K (Potassium): ')
+        Cl = input('Enter Cl ()')
+        CO2 = input('Enter CO2: ')
+        TP = input('Enter TP (): ')
+        Alb = input('Enter Alb (): ')
+        AG = input('Enter AG (): ')
+        Tbili = input('Enter Tbili (): ')
+        AlkPhos = input('Enter AlkPhos (): ')
+        AST = input('Enter AST (): ')
+        ALT = input('Enter ALT (): ')
+        Anion = input('Enter Anion (): ')
+        GFR = input('Enter GFR (): ')
+        Chol = input('Enter Chol (Cholesterol): ')
+        Trig = input('Enter Trig (Triglycerides): ')
+        HDL = input('Enter HDL: ')
+        LDL = input('Enter LDL: ')
+        VLDL = input('Enter VLDL: ')
+        c.execute("""INSERT INTO blood_test(date, WBC, RBC, HGB, HCT, 
+        MCV, MCH, MCH_C, RDW, PLT, MPV, Glu, BUN, BUNCreat,Calcium, 
+        Na, K, Cl, CO2, TP, Alb, AG, Tbili, AlkPhos, AST, ALT, Anion, 
+        GFR, Chol, Trig, HDL, LDL, VLDL) 
+        VALUES 
+        (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 
+        ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)""",
+        (date, WBC, RBC, HGB, HCT, MCV, MCH, MCH_C, RDW, PLT, MPV, Glu, BUN, BUNCreat, Calcium, Na, K, Cl, CO2, TP, Alb, AG, Tbili, AlkPhos, AST, ALT, Anion, GFR, Chol, Trig, HDL, LDL, VLDL))
+        conn.commit()
+
+    def read_from_db_blood_test():
+        c.execute('SELECT * FROM blood_test')
+        for row in c.fetchall():
+            print(row)
+
+    create_table_blood_test()
+    blood_test_dynamic_data_entry()
+    read_from_db_blood_test()
+
+    main_menu()
+
+def body_temperature():
+
+    def create_table_body_temperature():
+        c.execute('CREATE TABLE IF NOT EXISTS body_temperature(unix REAL, temperature REAL, feeling TEXT)')
+
+    def body_temperature_dynamic_data_entry():
+        unix = time.time()
+        temperature = input('Enter Body Temperature: ')
+        feeling = input('Enter how you are feeling today: ')
+        c.execute("INSERT INTO body_temperature(unix, temperature, feeling) VALUES (?, ?, ?)",
+                  (unix, temperature, feeling))
+        conn.commit()
+
+    def read_from_db_body_temperature():
+        c.execute('SELECT * FROM body_temperature')
+        for row in c.fetchall():
+            print(row)
+
+    create_table_body_temperature()
+    body_temperature_dynamic_data_entry()
+    read_from_db_body_temperature()
+
+    main_menu()
+
+def main_menu(): # Main Menu
+    print('')
+    print('1 - Blood Pressure')
+    print('2 - Blood Test Results')
+    print('3 - Weight, PWV, Bone Weight, Muscle Weight, Body Fat Percentage, Water')
+    print('4 - Body Temperature')
+    print('5 - Diet')
+    print('6 - Exercise')
+    print('7 - O2 Levels')
+    print('')
+
+    menu_selection = input('Make a selection: ')
+    print('')
+
+    if menu_selection == '1':
+        blood_pressure()
+    elif menu_selection == '2':
+        blood_test()
+    elif menu_selection == '3':
+        print('three')
+    elif menu_selection == '4':
+        body_temperature()
+    elif menu_selection == '5':
+        print ('four')
+    elif menu_selection == '6':
+        print ('four')
+    elif menu_selection == '7':
+        print ('four')
+    else:
+        print ('Unknown option selected, please choose again')
+        main_menu()
+    print('')
+
+main_menu()
+
+c.close()
+conn.close()
