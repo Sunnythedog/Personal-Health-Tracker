0e51bdfc193fbc81f3ba51a18b0c7fa87af8b8a3
Sunnythedog <16982432+Sunnythedog@users.noreply.github.com>
2018-02-11 20:17:29 -0500

    Update Health.py

--- Health.py
+++ Health.py
@@ -3,6 +3,7 @@
 
 import sqlite3
 import time
+import csv
 
 conn = sqlite3.connect('health.db')
 c = conn.cursor()
@@ -105,7 +106,8 @@ def blood_test():
         VALUES 
         (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 
         ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)""",
-        (date, WBC, RBC, HGB, HCT, MCV, MCH, MCH_C, RDW, PLT, MPV, Glu, BUN, BUNCreat, Calcium, Na, K, Cl, CO2, TP, Alb, AG, Tbili, AlkPhos, AST, ALT, Anion, GFR, Chol, Trig, HDL, LDL, VLDL))
+        (date, WBC, RBC, HGB, HCT, MCV, MCH, MCH_C, RDW, PLT, MPV, Glu, BUN, BUNCreat, Calcium, Na, K, Cl, CO2, TP, Alb,
+         AG, Tbili, AlkPhos, AST, ALT, Anion, GFR, Chol, Trig, HDL, LDL, VLDL))
         conn.commit()
 
     def read_from_db_blood_test():
@@ -284,6 +286,77 @@ def diet():
 
     diet_menu()
 
+# Upload Apple Health csv data to database
+
+def upload_apple_data():
+
+        c.execute('''CREATE TABLE IF NOT EXISTS apple_health_data(Start TEXT,Finish TEXT,Active Calories (kcal) REAL,
+        Biotin (mcg) REAL,Blood Alcohol Content (%) REAL,Blood Glucose (mg/dL) REAL,Blood Pressure (Diastolic) (mmHg) REAL,
+        Blood Pressure (Systolic) (mmHg) REAL,Body Fat Percentage (%) REAL,Body Mass Index (count) REAL,Body Temperature (degC) REAL,
+        Basal Body Temperature (degC) REAL,Caffeine (mg) REAL,Calcium (mg) REAL,Carbohydrates (mg) REAL,Chloride (mg) REAL,
+        Cholesterol (mg) REAL,Chromium (mcg) REAL,Copper (mcg) REAL,Cycling Distance (mi) REAL,Dietary Calories (cal) REAL,Distance (mi) REAL,
+        Electrodermal Activity (mcS) REAL,Fiber (g) REAL,Flights Climbed (count) REAL,Folate (mcg) REAL,Forced Expiratory Volume (L) REAL,
+        Forced Vital Capacity (L) REAL,Heart Rate (count/min) REAL,Inhaler Usage (count) REAL,Iodine (mcg) REAL,Lean Body Mass (lb) REAL,
+        Magnesium (mg) REAL,Manganese (mg) REAL,Molybdenum (mcg) REAL,Monosaturated Fat (g) REAL,Niacin (mg) REAL,NikeFuel (count) REAL,
+        Number of Times Fallen (count) REAL,Oxygen Saturation (%) REAL,Pantothenic Acid (mg) REAL,Peak Expiratory Flow Rate (L/min) REAL,
+        Peripheral Perfusion Index (%) REAL,Phosphorus (mg) REAL,Polyunsaturated Fat (g) REAL,Potassium (mg) REAL,Protein (g) REAL,
+        Respiratory Rate (count/min) REAL,Resting Calories (kcal) REAL,Riboflavin (mg) REAL,Saturated Fat (g) REAL,Selenium (mcg) REAL,
+        Sodium (mg) REAL,Steps (count) REAL,Sugar (g) REAL,Thiamin (mg) REAL,Total Fat (g) REAL,Vitamin A (mcg) REAL,Vitamin B12 (mcg) REAL,
+        Vitamin B6 (mg) REAL,Vitamin C (mg) REAL,Vitamin D (mcg) REAL,Vitamin E (mg) REAL,Vitamin K (mcg) REAL,Weight (lb) REAL,
+        Dietary Water (L) REAL,Zinc (mg) REAL,UV Exposure (count) REAL)''')
+
+        with open('Health Data.csv', 'rb') as fin:
+            dr = csv.DictReader(fin)
+            to_db = []
+            for i in dr:
+                to_db.append((i['Start'], i['Finish'], i['Active Calories (kcal)'], i['Biotin (mcg)'],
+                              i['Blood Alcohol Content (%)'],
+                              i['Blood Glucose (mg/dL)'], i['Blood Pressure (Diastolic) (mmHg)'],
+                              i['Blood Pressure (Systolic) (mmHg)'],
+                              i['Body Fat Percentage (%)'], i['Body Mass Index (count)'], i['Body Temperature (degC)'],
+                              i['Basal Body Temperature (degC)'],
+                              i['Caffeine (mg)'], i['Calcium (mg)'], i['Carbohydrates (mg)'], i['Chloride (mg)'],
+                              i['Cholesterol (mg)'],
+                              i['Chromium (mcg)'], i['Copper (mcg)'], i['Cycling Distance (mi)'],
+                              i['Dietary Calories (cal)'],
+                              i['Distance (mi)'], i['Electrodermal Activity (mcS)'], i['Fiber (g)'],
+                              i['Flights Climbed (count)'],
+                              i['Folate (mcg)'], i['Forced Expiratory Volume (L)'], i['Forced Vital Capacity (L)'],
+                              i['Heart Rate (count/min)'],
+                              i['Inhaler Usage (count)'], i['Iodine (mcg)'], i['Lean Body Mass (lb)'],
+                              i['Magnesium (mg)'], i['Manganese (mg)'],
+                              i['Molybdenum (mcg)'], i['Monosaturated Fat (g)'], i['Niacin (mg)'],
+                              i['NikeFuel (count)'], i['Number of Times Fallen (count)'],
+                              i['Oxygen Saturation (%)'], i['Pantothenic Acid (mg)'],
+                              i['Peak Expiratory Flow Rate (L/min)'], i['Peripheral Perfusion Index (%)'],
+                              i['Phosphorus (mg)'], i['Polyunsaturated Fat (g)'], i['Potassium (mg)'], i['Protein (g)'],
+                              i['Respiratory Rate (count/min)'],
+                              i['Resting Calories (kcal)'], i['Riboflavin (mg)'], i['Saturated Fat (g)'], i['Selenium (mcg)'],
+                              i['Sodium (mg)'],
+                              i['Steps (count)'], i['Sugar (g)'], i['Thiamin (mg)'], i['Total Fat (g)'], i['Vitamin A (mcg)'],
+                              i['Vitamin B12 (mcg)'],
+                              i['Vitamin B6 (mg)'], i['Vitamin C (mg)'], i['Vitamin D (mcg)'], i['Vitamin E (mg)'],
+                              i['Vitamin K (mcg)'], i['Weight (lb)'],
+                              i['Dietary Water (L)'], i['Zinc (mg)'], i['UV Exposure (count)']))
+
+            c.execute('''INSERT INTO apple_health_data(Start,Finish,Active Calories (kcal),Biotin (mcg),Blood Alcohol Content (%),
+                          Blood Glucose (mg/dL),Blood Pressure (Diastolic) (mmHg),Blood Pressure (Systolic) (mmHg),
+                          Body Fat Percentage (%),Body Mass Index (count),Body Temperature (degC),Basal Body Temperature (degC),
+                          Caffeine (mg),Calcium (mg),Carbohydrates (mg),Chloride (mg),Cholesterol (mg),Chromium (mcg),
+                          Copper (mcg),Cycling Distance (mi),Dietary Calories (cal),Distance (mi),Electrodermal Activity (mcS),
+                          Fiber (g),Flights Climbed (count),Folate (mcg),Forced Expiratory Volume (L),
+                          Forced Vital Capacity (L),Heart Rate (count/min),Inhaler Usage (count),Iodine (mcg),Lean Body Mass (lb),
+                          Magnesium (mg),Manganese (mg),Molybdenum (mcg),Monosaturated Fat (g),Niacin (mg),NikeFuel (count),
+                          Number of Times Fallen (count),Oxygen Saturation (%),Pantothenic Acid (mg),Peak Expiratory Flow Rate (L/min),
+                          Peripheral Perfusion Index (%),Phosphorus (mg),Polyunsaturated Fat (g),Potassium (mg),Protein (g),
+                          Respiratory Rate (count/min),Resting Calories (kcal),Riboflavin (mg),Saturated Fat (g),Selenium (mcg),
+                          Sodium (mg),Steps (count),Sugar (g),Thiamin (mg),Total Fat (g),Vitamin A (mcg),Vitamin B12 (mcg),
+                          Vitamin B6 (mg),Vitamin C (mg),Vitamin D (mcg),Vitamin E (mg),Vitamin K (mcg),Weight (lb),
+                          Dietary Water (L),Zinc (mg),UV Exposure (count)) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,
+                          ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);''', (to_db))
+
+            conn.commit()
+
 # Main Menu
 
 def main_menu():
@@ -296,6 +369,7 @@ def main_menu():
     print('5 - Diet')
     print('6 - Exercise')
     print('7 - O2 Levels')
+    print('8 - Upload Apple Health Data')
     print('q - To Quit')
     print('')
 
@@ -316,6 +390,8 @@ def main_menu():
         print ('six')
     elif menu_selection == '7':
         print ('seven')
+    elif menu_selection == '8':
+        upload_apple_data()
     elif menu_selection == 'q':
         print('quit')
     else:
